9fbdc515b8034ef955063cca2468a81c83b006e9
sedx876 <swatkins876@gmail.com>
2020-10-26 07:58:49 -0400

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import './styles/App.css';
+import { Admin, Resource } from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/PostList'
+import PostCreate from './components/PostCreate'
+import PostEdit from './components/PostEdit'
+import UserList from './components/UserList'
+import UserCreate from './components/UserCreate'
+import UserEdit from './components/UserEdit'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Resource
+        name='posts'
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      />
+      <Resource
+        name='users'
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
+      />
+    </Admin>
+  )
 }
 
-export default App;
+export default App
